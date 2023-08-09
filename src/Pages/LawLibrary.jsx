0b8e2ceb6ad77b23a511f2951f936001b1c9c96c
Rabeeh <rabirabz001@gmail.com>
2023-08-09 15:30:20 +0530

    add histor component

--- src/Pages/LawLibrary.jsx
+++ src/Pages/LawLibrary.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Wrapper from '../Components/Wrapper'
 
 import OngoingResearches from '../Components/OngoingResearches';
 import { AiOutlineBank } from "react-icons/ai";
+import LawLbryHistory from '../Components/LawLbryHistory';
 
 
 const LawLibrary = () => {
@@ -12,16 +13,22 @@ const LawLibrary = () => {
         icon: <AiOutlineBank/>,
         parentPath: 'Law Library'
     }
+    const [page,setPage]=useState(true)
+    const handlePage =(value)=>{
+       value?setPage(value):setPage(value)
+    }
     return (
         <Wrapper TaskBarData={TaskBarData} >
             <div className='h-14 w-[370px] rounded-xl bg-white mx-auto  flex  justify-center items-center mt-4'>
                 <div className=' grid grid-cols-2 w-[95%] gap-1 h-[80%]'>
-                    <div className=' bg-black rounded-xl  flex items-center justify-center  font-semibold text-[#a7a9b7] text-sm'><button>On Going Researches</button></div>
-                    <div className='bg-white rounded-xl  flex items-center justify-center hover:bg-slate-100'><button className='text-[#a7a9b7] text-sm'>History</button></div>
+                    <div onClick={()=>handlePage(true)} className={ `${page?`bg-black`:`bg-white`}   rounded-xl flex items-center justify-center  font-semibold text-[#a7a9b7] text-sm`}><button>On Going Researches</button></div>
+                    <div onClick={()=>handlePage(false)} className={ `${page?`bg-white`:`bg-black`} rounded-xl  flex items-center justify-center font-semibold`}><button className='text-[#a7a9b7] text-sm'>History</button></div>
                 </div>
             </div>
            
-            <OngoingResearches/>
+            
+            {page?<OngoingResearches/>: <LawLbryHistory/>}
+           
 
 
 
