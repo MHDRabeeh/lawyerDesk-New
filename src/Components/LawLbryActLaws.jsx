eb2c535a407ee014d052f74f175ad9d4d0b149cb
Rabeeh <rabirabz001@gmail.com>
2023-08-13 05:21:46 +0530

    update law paga

--- src/Components/LawLbryActLaws.jsx
+++ src/Components/LawLbryActLaws.jsx
@@ -1,10 +1,111 @@
 import React from 'react'
+import { MdFileCopy } from "react-icons/md";
+import { FaShare } from "react-icons/fa";
 
 const LawLbryActLaws = () => {
+
+
   return (
-    <div>
-      2
-    </div>
+    <>
+      <div className='w-[97%] h-[54vh]  flex flex-col justify-between bg-white  -mt-0 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl'>
+        <div className='w-[95%] mx-auto mt-3 flex justify-between  '>
+          <p className=' text-left text-lg font-bold'>Acts, Laws, and Regulations</p>
+          <span className='text-3xl text-[#407bff]'><MdFileCopy /></span>
+        </div>
+        <div className='flex  justify-between  plac  w-[95%] h-[43vh] gap-10 overflow-auto scrollbar-hide mx-auto  rounded-2xl mt-3'>
+
+          <div className='flex flex-col gap-3'>
+
+            <div className='border border-slate-100 rounded-2xl w-[30vw] h-32  shadow-sm'>
+              <div className='w-[95%] mx-auto flex flex-col   h-[100%] justify-evenly '>
+                <div className='flex justify-between'> <span className='font-bold'>Motor Vehicles Act, 1988</span><span className='text-2xl text-[#407bff]'><FaShare /></span></div>
+                <div>
+                  <p className='flex text-xs'><span className='font-bold'>a . </span> Section 112 (Speed Limits) </p>
+                  <p className='flex text-xs'><span className='font-bold'>b . </span>Section 134 (Duty of driver in case of accident and injury to a person)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 166 (Claim for compensation on accident)</p>
+                  <p className='flex text-xs'><span className='font-bold'>d . </span>Section 184 (Driving dangerously)</p>
+                </div>
+              </div>
+            </div>
+            <div className='border border-slate-100 rounded-2xl w-[30vw] h-28  shadow-sm'>
+              <div className='w-[95%] mx-auto flex flex-col   h-[100%] justify-evenly '>
+                <div className='flex justify-between'> <span className='font-bold'>Central Motor Vehicles Rules, 1989</span><span className='text-2xl text-[#407bff]'><FaShare /></span></div>
+                <div>
+                  <p className='flex text-xs'><span className='font-bold'>a . </span>  Rule 93 (Maintenance of motor vehicles) </p>
+                  <p className='flex text-xs'><span className='font-bold'>b . </span>Rule 118 (Safety glass)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Rule 119 (Brakes)</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='border border-slate-100 rounded-2xl w-[30vw] h-40  shadow-sm'>
+              <div className='w-[95%] mx-auto flex flex-col   h-[100%] justify-evenly '>
+                <div className='flex justify-between'> <span className='font-bold'>Indian Penal Code (IPC), 1860</span><span className='text-2xl text-[#407bff]'><FaShare /></span></div>
+                <div>
+                  <p className='flex text-xs'><span className='font-bold'>a . </span>  Section 279 (Rash driving or riding on a public way) </p>
+                  <p className='flex text-xs'><span className='font-bold'>b . </span>Section 304A (Causing death by negligence)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 336 (Act endangering life or personal safety of others)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 337 (Causing hurt by an act endangering life or personal safety of others)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 338 (Causing grievous hurt by an act endangering life or personal safety of others)</p>
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+          <div className='flex flex-col gap-3'>
+
+            <div className='border border-slate-100 rounded-2xl w-[30vw] h- shadow-sm'>
+              <div className='w-[95%] mx-auto flex flex-col   h-[100%] justify-evenly '>
+                <div className='flex justify-between'> <span className='font-bold'>Motor Vehicles Act, 1988</span><span className='text-2xl text-[#407bff]'><FaShare /></span></div>
+                <div>
+                  <p className='flex text-xs'><span className='font-bold'>a . </span> Section 112 (Speed Limits) </p>
+                  <p className='flex text-xs'><span className='font-bold'>b . </span>Section 134 (Duty of driver in case of accident and injury to a person)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 166 (Claim for compensation on accident)</p>
+                  <p className='flex text-xs'><span className='font-bold'>d . </span>Section 184 (Driving dangerously)</p>
+                </div>
+              </div>
+            </div>
+            <div className='border border-slate-100 rounded-2xl w-[30vw] h-24  shadow-sm'>
+              <div className='w-[95%] mx-auto flex flex-col   h-[100%] justify-evenly '>
+                <div className='flex justify-between'> <span className='font-bold'>Central Motor Vehicles Rules, 1989</span><span className='text-2xl text-[#407bff]'><FaShare /></span></div>
+                <div>
+                  <p className='flex text-xs'><span className='font-bold'>a . </span>  Rule 93 (Maintenance of motor vehicles) </p>
+                  <p className='flex text-xs'><span className='font-bold'>b . </span>Rule 118 (Safety glass)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Rule 119 (Brakes)</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='border border-slate-100 rounded-2xl w-[30vw] h-36  shadow-sm'>
+              <div className='w-[95%] mx-auto flex flex-col   h-[100%] justify-evenly '>
+                <div className='flex justify-between'> <span className='font-bold'>Indian Penal Code (IPC), 1860</span><span className='text-2xl text-[#407bff]'><FaShare /></span></div>
+                <div>
+                  <p className='flex text-xs'><span className='font-bold'>a . </span>  Section 279 (Rash driving or riding on a public way) </p>
+                  <p className='flex text-xs'><span className='font-bold'>b . </span>Section 304A (Causing death by negligence)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 336 (Act endangering life or personal safety of others)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 337 (Causing hurt by an act endangering life or personal safety of others)</p>
+                  <p className='flex text-xs'><span className='font-bold'>c . </span>Section 338 (Causing grievous hurt by an act endangering life or personal safety of others)</p>
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+
+
+        </div>
+        <div className=' text-xs flex justify-end w-[98%] text-[#a7a9b7]'><span>29/2000</span></div>
+      </div>
+      <div className='flex justify-between  items-center  mt-3 w-[95%] mx-3 '>
+        <div className='font-bold '>Not what you were expecting?</div>
+        <div className='flex justify-center items-center '> <button className='px-40 py-3 font-semibold rounded-3xl text-white bg-[#407bff]'>Regenerate</button></div>
+      </div>
+
+
+    </>
   )
 }
 
