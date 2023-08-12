57e5a86894eae1daf2dcc3cf01ad7f4857a25999
Rabeeh <rabirabz001@gmail.com>
2023-08-12 21:30:11 +0530

    add low modal

--- src/Components/ModalLaw.jsx
+++ src/Components/ModalLaw.jsx
@@ -1,51 +1,44 @@
 import React from 'react'
+import { RxCross2 } from "react-icons/rx";
+import { TbPointFilled } from 'react-icons/tb'
+import image1 from '../assets/Modalimg.jpg'
+import { RiSendPlane2Fill } from 'react-icons/ri'
+const ModalLaw = ({ setModal }) => {
+    return (
+        <div className='fixed  inset-0  flex justify-center items-center bg-black bg-opacity-80'>
+            <div className='w-[30vw] h-[50vh] bg-white rounded-3xl '>
+                <div className=' flex justify-end '>
+                    <div className='p-1 bg-[#407BFF] m-3 rounded-full'>
+                        <span onClick={() => setModal((pre) => false)} className='text-white cursor-pointer text-xl font-semibold rounded-full'><RxCross2 /></span>
+                    </div>
+                </div>
+                <div className='flex flex-col justify-between h-[82%]'>
+                    <div className='w-[70%] mx-auto flex gap-3'>
+                        <div>
+                            <div><img src={image1} className='w-20' alt="" /></div>
+                        </div>
+                        <div className='flex flex-col gap-3'>
+                            <span className='text-xl font-bold'>Need to fix..!</span>
+                            <span><button className='text-xs px-3 py-2 bg-[#407bff] text-white rounded-3xl'>Retry (max 1)</button></span>
+                        </div>
+                    </div>
+                    <div className='w-[70%] mx-auto'>
+                        {
+                            ["In the legal case involving a vehicular", "the driver, hereby referred to as the", "contends that they exercised due care"].map((item, i) => (
+                                <p className='flex items-center text-sm ' key={i}> <span><TbPointFilled /></span>{item}</p>
+                            ))
+                        }
+                    </div>
 
-const ModalLaw = () => {
-  return (
-    <>
-
-    
-{/* <!-- Modal toggle --> */}
-
-
-{/* <!-- Main modal --> */}
-<div id="defaultModal" tabindex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div className="relative w-full max-w-2xl max-h-full">
-        {/* <!-- Modal content --> */}
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            {/* <!-- Modal header --> */}
-            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Terms of Service
-                </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span className="sr-only">Close modal</span>
-                </button>
+                    <div className='w-[90%] mx-auto flex items-center gap-2'>
+                        <input type="text" className='w-[80%] mx-auto border-slate-200 h-9 rounded-full bg-slate-100 placeholder text-sm placeholder-slate-300 px-4' placeholder='Enter details...' />
+                        <div className='bg-slate-100 rounded-full p-2'><span className='text-[#407bff] text-2xl'><RiSendPlane2Fill /></span></div>
+                    </div>
+                </div>
             </div>
-            {/* <!-- Modal body --> */}
-            <div className="p-6 space-y-6">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-            </div>
-            {/* <!-- Modal footer --> */}
-            <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                <button data-modal-hide="defaultModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-            </div>
+
         </div>
-    </div>
-</div>
-
-      
-    </>
-  )
+    )
 }
 
 export default ModalLaw
